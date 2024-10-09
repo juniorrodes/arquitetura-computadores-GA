8809fd414d82c937807b2ba87f260d65b9bb91d9
juniorrodes <juniorrodes@gmail.com>
2024-10-08 21:06:37 -0300

    Parse do arquivo de teste

--- pkg/instructions/instruction.go
+++ pkg/instructions/instruction.go
@@ -1,13 +1,15 @@
 package instructions
 
-import "slices"
-
-const SUPPORTED_INSTRUCTIONS = []string{ "add", "sum", "beq", "lw", "sw", "noop", "halt" }
-
 import (
 	"fmt"
 	"strconv"
 	"strings"
+    "slices"
+)
+
+var (
+    SUPPORTED_INSTRUCTIONS = []string{ "add", "sum", "beq", "lw", "sw", "noop", "halt" }
+    labels map[string]int = make(map[string]int)
 )
 
 type MemInstruction struct {
@@ -21,33 +23,57 @@ func ParseInstructions(buffer []byte) (instructions []MemInstruction, err error)
     str := string(buffer)
     str = strings.TrimRight(str, "\n")
     lines :=  strings.Split(str, "\n")
-
-    for _, line := range lines {
+    
+    for i, line := range lines {
         lineParsed := strings.Split(line, " ")
-        if slices.Contains(SUPPORTED_INSTRUCTIONS, lineParsed[0]) {
-            operands := make([]int, 3)
-
-            for i := 1; i < 4; i++ {
-                operands[i - 1], err = strconv.Atoi(lineParsed[i])
-                if err != nil {
-                    return
-                }
+        if !slices.Contains(SUPPORTED_INSTRUCTIONS, lineParsed[0]) {
+            if _, ok := labels[lineParsed[0]]; !ok {
+                labels[lineParsed[0]] = i
+                lines[i] = strings.ReplaceAll(lines[i], lineParsed[0], "")
             }
-
-            instruction := MemInstruction{
-                OpCode: lineParsed[0],
-                Operand1: operands[0],
-                Operand2: operands[1],
-                Operand3: operands[2],
-            }
-
-            instructions = append(instructions, instruction)
         }
     }
 
+    for _, line := range lines {
+        line = strings.TrimLeft(line, " ")
+        lineParsed := strings.Split(line, " ")
+        var instruction MemInstruction
+        instruction, err = newInstruction(lineParsed)
+        if err != nil {
+            return
+        }
+        instructions = append(instructions, instruction)
+     }
     return
 }
 
+func newInstruction(line []string) (MemInstruction, error) {
+    operands := make([]int, 3)
+    var err error
+    
+    if !slices.Contains(SUPPORTED_INSTRUCTIONS[4:], line[0]) {
+        for i := 1; i < 4; i++ {
+            operands[i - 1], err = strconv.Atoi(line[i])
+            if err != nil {
+                if v, ok := labels[line[i]]; ok {
+                    operands[i - 1] = v
+                } else {
+                    return MemInstruction{}, err
+                }
+            }
+        }
+    }
+
+    instruction := MemInstruction{
+        OpCode: line[0],
+        Operand1: operands[0],
+        Operand2: operands[1],
+        Operand3: operands[2],
+    }
+
+    return instruction, nil
+} 
+
 func (m *MemInstruction) String() string {
-    return fmt.Sprintf("{\n\t\"opCode\": %s,\n\t\"operand1\": %d,\n\t\"operand2\": %d,\n\t\"operand3\": %d,\n}", m.OpCode, m.Operand1, m.Operand2, m.Operand3)
+        return fmt.Sprintf("{\n\t\"opCode\": %s,\n\t\"operand1\": %d,\n\t\"operand2\": %d,\n\t\"operand3\": %d,\n}", m.OpCode, m.Operand1, m.Operand2, m.Operand3)
 }
--- test.asm
+++ test.asm
@@ -1,3 +1,16 @@
 lw 0 1 -1
 lw 0 2 10
 lw 0 3 1
+noop
+loop add 2 1 2
+noop
+noop
+beq 2 0 done
+noop
+noop
+noop
+beq 0 0 loop
+noop
+noop
+noop
+done halt
